5ab8bfbdea4a692aa1ba28d425ec6ba4248d3b74
Chellakumar0310 <85168310+Chellakumar0310@users.noreply.github.com>
2021-06-08 20:07:05 +0530

    SimpleInterest
    Here the simple interest is calculated and displayed on the screen.

---
+++ SimpleInterest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double Principal,RateOfInterest;
+		int NoOfYears;
+		double SimpleInterest;
+		
+		Scanner in = new Scanner(System.in);
+		
+		//Prompt for Principal amount and accept the Principal value.
+		System.out.print("\nEnter the Principal amount:");
+		Principal = in.nextDouble();
+		
+		//Prompt for Rate of Interest and accept the rate of interest
+
+	}
+
+}
